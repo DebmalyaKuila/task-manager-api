bb40cdadf09343522c97420ae960a0466873e250
Debmalya <debmalyakuila911@gmail.com>
2024-01-13 14:48:44 +0530

    integrating timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,23 +1,30 @@
-const mongoose =require("mongoose")
-const validator=require("validator")
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+//Task schema
+const taskSchema = new mongoose.Schema(
+    {
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'user'
+        }
+    },
+    {
+        timestamps:true
+    }
+)
 
 //Task model
-const Task =mongoose.model("Tasks",
-{
-description :{
-    type : String,
-    required:true,
-    trim :true
-},
-completed :{
-    type: Boolean,
-    default:false
-},
-owner:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true,
-    ref:'user'
-}
-})
+const Task = mongoose.model("Tasks", taskSchema)
 
-module.exports=Task
+module.exports = Task
